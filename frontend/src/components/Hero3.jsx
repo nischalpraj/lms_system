fe6a5517ca3f1b89568083d9f3c5cc6f96442422
nischalpraj <nischalpraj776@gmail.com>
2026-05-02 20:08:07 +0545

    Adding quote

--- frontend/src/components/Hero3.jsx
+++ frontend/src/components/Hero3.jsx
@@ -4,15 +4,13 @@ import "./Hero3.css";
 
 const Hero3 = () => {
   return (
-      <div className='big-div'>
-          <div className='icon-quote'>
-              <img src={icon}/>
-          </div>
-          <div className='quote'>
-              
-          </div>
+    <div className="big-div">
+      <div className="icon-quote">
+        <img src={icon} />
+      </div>
+      <div className="quote">Proof, not promises</div>
     </div>
-  )
+  );
 }
 
 export default Hero3
