741fff43a74645fcfe18cea90c610236b70bb2b9
nischalpraj <nischalpraj776@gmail.com>
2026-05-02 20:05:58 +0545

    Making of hero 3

---
+++ frontend/src/components/Hero3.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import icon from "./icon-quote.svg"
+import "./Hero3.css";
+
+const Hero3 = () => {
+  return (
+      <div className='big-div'>
+          <div className='icon-quote'>
+              <img src={icon}/>
+          </div>
+          <div className='quote'>
+              
+          </div>
+    </div>
+  )
+}
+
+export default Hero3
